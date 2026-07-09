d64b54720b23121844c2af566967ad8bc0b02ce2
Cristiano Abilio <cristianocafr@gmail.com>
2026-07-08 21:03:56 -0300

    feat: integrating OpenAI

--- app/Models/SaleCommission.php
+++ app/Models/SaleCommission.php
@@ -9,4 +9,19 @@ class SaleCommission extends Model
     protected $table = 'sales_commission_view';
     public $timestamps = false;
     public $incrementing = false;
+
+    public function ScopeGetColumns()
+    {
+        return [
+            'company',
+            'seller',
+            'client',
+            'sold_at',
+            'total_amount',
+            'status',
+            'city',
+            'state',
+            'commission',
+        ];
+    }
 }
